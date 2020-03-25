54176e6fa7864b50086dc2f0cbd8f775f6630613
Zach S.L <zach.s.ledford@gmail.com>
2020-03-25 17:48:09 -0400

    added for loop

--- assets/javascript/webster.js
+++ assets/javascript/webster.js
@@ -1,19 +1,44 @@
-// $(".input-group-prepend").click(function (event) {
 
-//     event.preventDefault();
-//     $(this).remove(".card-title");
-//     $(this).remove(".card-text")
 
-//     var wordSearch = $(".form-control").val().trim();
+
+$(document).ready(function() {
+  $(".input-group-text").click(function(event) {
+    $(".form-control").keypress();
+
+    event.preventDefault();
+    $(this).remove(".card-title");
+    $(this).remove(".card-text");
+
+    var wordSearch = $(".form-control")
+      .val()
+      .trim();
 
     // My API key: 56e8e2c7-0a6a-4cc2-8060-7e81c4e3e03
-    var queryURL = "https://www.dictionaryapi.com/api/v3/references/learners/json/" + wordSearch + "?key=56e8e2c7-0a6a-4cc2-8060-7e81c4e3e038";
-    console.log(queryURL)
+    var queryURL =
+      "https://www.dictionaryapi.com/api/v3/references/learners/json/" + wordSearch + "?key=137bad9b-3731-45ab-ae8c-584bd5cae8d5";
+    console.log(queryURL);
     $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-       
-    })
-// })
+      url: queryURL,
+      method: "GET",
+    }).then(function(response) {
+      
+        var el = response;
+        console.log(response)
+        var audio_link = `https://media.merriam-webster.com/soundc11/${wordSearch[0]}/${response[0].hwi.prs[0].sound.audio}.wav`
+        console.log(audio_link)
+        // First Definition
+        $(".card-title").text(el[0].hwi.hw);
+        // $("#nes-wordClass").text(el[0].fl);
+        $(".card-text").text(el[0].shortdef[0]);
+
+        for (var i=0; i < 4; i++){
+            console.log(i)
+        }
+      
+    });
+  });
+
+  
+
+
+})
