056bd0f751c0576a014f7a9550b7cf0206985ab1
Haylee McLemore <hayleemclemore@Haylees-MBP.attlocal.net>
2020-03-28 14:50:20 -0500

    collocation append and scroll feature

--- assets/javascript/webster.js
+++ assets/javascript/webster.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
+
   $("#word-input").on("keypress", function(event) {
     if (event.which === 13) {
       event.preventDefault();
       $("#def-coll").empty();
+      $("#collocation-result").empty();
+      $([document.documentElement,document.body]).animate({scrollTop: $("#collocation").offset().top}, 800);
       
 
       var wordSearch = $("#word-input")
@@ -20,12 +23,26 @@ $(document).ready(function() {
         url: queryURL,
         method: "GET"
       }).then(function(response) {
+        console.log(response)
         var el = response;
+        
 
         // Access the sound URL from the API
         var audio_link = `https://media.merriam-webster.com/soundc11/${wordSearch[0]}/${el[0].hwi.prs[0].sound.audio}.wav`;
 
+        // Display word searched with audio icon
+        $("#def-coll").append(
+          `
+          <div class="word-searched">
+          <h2 class="card-title">${wordSearch}</h2>
+          <i data-sound=${audio_link} class="fas fa-volume-up sound"></i>
+          </div>
+          `
+        );
+
+
         for (var i = 0; i < 4; i++) {
+        
         
           // Loop through the different parts of speech
           if (
@@ -35,21 +52,51 @@ $(document).ready(function() {
             el[i].fl === "adverb"
           ) {
             if (el[i].meta.stems[0] === wordSearch) {
-              $("#def-coll").append(
-                `<h5 class="card-title">${i + 1}. ${wordSearch}</h5>`
-              );
+              
 
               $("#def-coll").append(
-                // `<audio controls><source src="${audio_link}" type="audio/wav"><i class="fas fa-volume-up"></i></audio>`
-                `<i data-sound=${audio_link} class="fas fa-volume-up sound"></i>`
-              );
-
-              $("#def-coll").append(
-                `<p class="card-text">${el[i].shortdef[0]}</p>`
+                `<p class="card-text">${i + 1}. ${el[i].shortdef[0]}</p>`
               );
             }
           }
         }
+
+        for (var i = 0; i < 10; i++) {
+
+
+        };
+
+      });
+
+      var APIkey = "d3749e9e16msh9f34dc06d606f25p158454jsn2925b80b7492";
+      var collocationQueryURL =
+        "https://linguatools-english-collocations.p.rapidapi.com/bolls/?lang=en&query=" + wordSearch;
+    
+      $.ajax({
+        url: collocationQueryURL,
+        method: "GET",
+        async: true,
+        crossDomain: true,
+        headers: {
+          "x-rapidapi-host": "linguatools-english-collocations.p.rapidapi.com",
+          "x-rapidapi-key": APIkey
+        }
+
+      // Adding collocations to page
+      }).then(function(response) {
+        console.log(response)
+        for (var i = 0; i < 10; i++ ) {
+          var li = $("#collocation-result").append(`<li></li>`);
+        
+          $(li).append(`<h5>${response[i].collocation}</h5>`);
+
+          response[i].examples.forEach(function(val){
+
+            $(li).append(`<p>${val}</p>`);
+          })
+         
+        }
+  
       });
     }
   });
